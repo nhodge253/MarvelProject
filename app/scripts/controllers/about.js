4ec2f6be26e0872a6fb54b6e6a018096677afbb4
shao sosa <shao sosa>
2017-02-08 00:39:35 -0800

    1st workthrough completed. Still figuring out About tab

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name marvelProjectApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the marvelProjectApp
+ */
+angular.module('marvelProjectApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name marvelProjectApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the marvelProjectApp
+ */
+angular.module('marvelProjectApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
